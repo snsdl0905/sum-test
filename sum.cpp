fc062727929b29d77cc74fa6ca0bf92deb2bcf71
snsdl0905 <snsdl0905@naver.com>
2025-04-02 00:35:40 -0500

    Update cum.cpp

--- sum.cpp
+++ sum.cpp
@@ -2,9 +2,10 @@
 #include "sum.h"
 
 int sum(int n) {
-	int res = 0;
-	for (int i = 1; i <= n; ++i)
-		res += i;
-	return res;
+	return n * (n + 1) / 2;
+//	int res = 0;
+//	for (int i = 1; i <= n; ++i)
+//		res += i;
+//	return res;
 }
 
